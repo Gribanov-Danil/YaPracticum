a57437ceef994123c24d41c3a8f5e06a08286724
Danil Gribanov <gribanovwork@yandex.ru>
2023-02-07 20:04:32 +0500

    fix: добавил ссылки в header

--- react-burger/src/components/navBarItem/NavBarItem.jsx
+++ react-burger/src/components/navBarItem/NavBarItem.jsx
@@ -7,13 +7,15 @@ import {IconfyText} from "../iconfyText/IconfyText";
 export const NavBarItem = ({textClass, text, icon}) => {
     return (
         <div className={`pl-5 pr-5 pb-4 pt-4 ${headerClasses.navbarItem}`}>
-            <IconfyText
-                text={text}
-                textClass={textClass}
-                iconLocation="Left"
-                gapInPx={8}
-                icon={icon}
-            />
+            <a href="#">
+                <IconfyText
+                    text={text}
+                    textClass={textClass}
+                    iconLocation="Left"
+                    gapInPx={8}
+                    icon={icon}
+                />
+            </a>
         </div>
     )
 }
