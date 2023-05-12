0a1f8958be4eb0653374663e00411627051bb19d
Danil Gribanov <gribanovwork@yandex.ru>
2023-05-13 02:46:32 +0500

    feat: реордеринг элементов коснтруктора происходит при взаимодействии с иконкой

--- src/components/constructor-ingredient/constructor-ingredient.tsx
+++ src/components/constructor-ingredient/constructor-ingredient.tsx
@@ -1,10 +1,9 @@
 import panelStyles from "../constructor-panel/constructor-panel.module.css"
 import { deleteDraggableIngredient } from "../../service/reducers/picked-ingredients-slice/picked-ingredients-slice"
 import { memo } from "react"
-import { Reorder } from "framer-motion"
+import { Reorder, useDragControls } from "framer-motion"
 import { TIngredientObj } from "../../utils/models/ingredient-types/types"
 import { useAppDispatch } from "../../hooks/redux"
-import { getScreenType } from "../../utils/getScreenType"
 import { ConstructorElement } from "../../ui/constructor-element/constructor-element"
 import { DragIcon } from "@ya.praktikum/react-developer-burger-ui-components"
 
@@ -18,16 +17,31 @@ export const ConstructorIngredient = memo<IConstructorIngredient>(function Const
 }) {
   const dispatch = useAppDispatch()
   const handleClose = () => dispatch(deleteDraggableIngredient(ingredientObj))
+  const controls = useDragControls()
 
   return (
-    <Reorder.Item value={ingredientObj} className={panelStyles.ingredient}>
-      <DragIcon type="primary" />
+    <Reorder.Item
+      value={ingredientObj}
+      className={panelStyles.ingredient}
+      dragListener={false}
+      dragControls={controls}
+    >
+      <div
+        className={`${panelStyles.test} reorder-handle`}
+        style={{ touchAction: "none" }}
+        onPointerDown={(e) => {
+          console.log(e)
+          controls.start(e)
+        }}
+      >
+        <DragIcon type="primary" />
+      </div>
+
       <ConstructorElement
         text={ingredientObj.ingredient.name}
         price={ingredientObj.ingredient.price}
         thumbnail={ingredientObj.ingredient.image_mobile}
         handleClose={handleClose}
-        extraClass={getScreenType() === "desktop" ? "" : panelStyles.mobile_constructor}
       />
     </Reorder.Item>
   )
