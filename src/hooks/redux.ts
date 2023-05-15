77674d531ef04a217200d776ca169554fc0d6654
Danil Gribanov <gribanovwork@yandex.ru>
2023-05-16 01:26:15 +0500

    refactoring: добавил описание хуков

--- src/hooks/redux.ts
+++ src/hooks/redux.ts
@@ -1,5 +1,7 @@
 import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux"
 import { TAppDispatch, TRootState } from "../service/store"
 
+// useDispatch с типизацией
 export const useAppDispatch = () => useDispatch<TAppDispatch>()
+// useSelector с типизацией
 export const useAppSelector: TypedUseSelectorHook<TRootState> = useSelector
--- src/hooks/use-form.ts
+++ src/hooks/use-form.ts
@@ -1,5 +1,15 @@
 import { ChangeEvent, useState } from "react"
 
+/**
+ * Универсальный хук для форм
+ * @param inputValues начальные значения формы
+ *
+ * @returns {values, handleChange, setValues, setValue} возвращаемые значения:
+ * values - объект, содержащий значение полей формы
+ * handleChange - хендлер для изменения любого поля формы
+ * setValues - изменение нескольких полей вручную
+ * setValue - изменение конкретного поля вручную
+ */
 export function useForm<T extends object>(inputValues: T) {
   const [values, setValues] = useState<T>(inputValues)
   const setValue = (name: string, value: string) => {
